cbaade4b15524e912842c2b8ddc284587872dfda
snopeks <creativesnopeks@gmail.com>
2018-05-02 08:50:06 -0700

    add car class

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private int c;
+    private float xpos;
+    private float ypos;
+    private float xspeed;
+
+    public Car(int tempC, float tempXpos, float tempYpos, float tempXspeed ) {
+        c = tempC;
+        xpos = tempXpos;
+        ypos = tempYpos;
+        xspeed = tempXspeed;
+    }
+
+    public void move(){
+        //here is the car move method. Will fill in later.
+    }
+
+}
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,8 +1,15 @@
 import processing.core.PApplet;
+
 public class MainApp extends PApplet {
     public static void main(String[] args){
         PApplet.main("MainApp", args);
     }
+    float x = 0;
+    float y = 100;
+    float speed = 2;
+    float c = color(0);
+    Car car1;
+    Car car2;
 
     public void settings(){
         size(600, 600);
@@ -10,16 +17,33 @@ public class MainApp extends PApplet {
 
     public void setup(){
         smooth();
-        background(0);
+        background(225);
+    }
+    public void display(){
+        fill(200);
+        rect(x, y,100, 30, 10);
+
+    }
+
+    public void move() {
+        x = x + speed;
+        if (x > width) {
+            x = 0;
+        }
     }
 
     public void draw(){
         noFill();
+        move();
+        display();
 
-        stroke(0, random(128, 255), 0, 64);
-        strokeWeight(random(1, 4) );
-
-//        fill(255, 0, 255);
-        ellipse(random (width), random(height), 30, 30);
+        stroke(0, random(128, 255), 0);
+//        strokeWeight(random(1, 4) );
+//        ellipse(mouseX, mouseY, 33, 33);
+//        ellipse(mouseX, mouseY, 130, 130);    // Top circle
+//        ellipse(mouseX+20, mouseY, 50, 50); // Middle circle
     }
 }
+
+
+
